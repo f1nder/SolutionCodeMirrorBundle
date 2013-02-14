aecdeb024c1edfd862403cb91ae4b3cae6610dfd
aleks <aleks@asdasd>
2013-02-14 22:52:41 +0200

    tests

--- Tests/AppKernel.php
+++ Tests/AppKernel.php
@@ -14,12 +14,8 @@ class AppKernel extends Kernel
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
             new \Symfony\Bundle\MonologBundle\MonologBundle(),
-            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new \JMS\AopBundle\JMSAopBundle(),
-            new \JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new \Solution\CodeMirrorBundle\SolutionCodeMirrorBundle(),
     )
         ;
